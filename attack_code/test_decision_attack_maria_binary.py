6ef8c7d41d6a0c80bb19c1c854e29503ba13d0ef
root <root@britney-dbreach-image-26.us-central1-a.c.turing-variety-306620.internal>
2024-04-18 17:17:58 +0000

    binary search files

--- attack_code/test_decision_attack_maria_binary.py
+++ attack_code/test_decision_attack_maria_binary.py
@@ -19,31 +19,32 @@ control.create_basic_table(table,
         encrypted=True)
 
 possibilities = []
+
 if sys.argv[1] == "--random":
-    for _ in range(2000):
-        size = np.random.randint(10, 20)
-        secret = "".join(random.choices(string.ascii_lowercase, k=size))
-        possibilities.append(secret)
+    with open("/home/britney/dbreach-britney/resources/random-dataset.txt") as f:
+        for line in f:
+            word = line.strip().lower()
+            possibilities.append(word)
 if sys.argv[1] == "--english":
-    with open("./resources/10000-english-long.txt") as f:
+    with open("/home/britney/dbreach-britney/resources/english-dataset.txt") as f:
         for line in f:
             word = line.strip().lower()
             possibilities.append(word)
 if sys.argv[1] == "--emails":
-    with open("./resources/fake-emails.txt") as f:
+    with open("/home/britney/dbreach-britney/resources/emails-dataset.txt") as f:
         for line in f:
             email = line.strip().lower()
             possibilities.append(email)
 
 print("true_label,num_secrets,b_no,b_guess,b_yes,setup_time,per_guess_time")
 
-secrets_to_try = [1, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240]
+secrets_to_try = [100]
 secrets_to_try.reverse()
 startAttack = time.time()
 for num_secrets in secrets_to_try:
-    random.shuffle(possibilities)
-    for trial in range(0, 200, num_secrets):
-        startRound = time.time()
+    # random.shuffle(possibilities)
+    for trial in range(0, 10):
+        trial_possibilities = possibilities[0:200]
         success = False
         control.drop_table(table)
         control.create_basic_table(table,
@@ -53,19 +54,21 @@ for num_secrets in secrets_to_try:
         guesses = []
         correct_guesses = set()
         for secret_idx in range(num_secrets):
-            secret = possibilities[(trial + secret_idx) % len(possibilities)]
+            secret = trial_possibilities[(trial + secret_idx) % len(trial_possibilities)]
             control.insert_row(table, secret_idx, secret)
             guesses.append(secret)
             correct_guesses.add(secret)
 
         for secret_idx in range(num_secrets, num_secrets*2):
-            wrong_guess = possibilities[(trial + secret_idx) % len(possibilities)]
+            wrong_guess = trial_possibilities[(trial + secret_idx) % len(trial_possibilities)]
             guesses.append(wrong_guess)
 
         fillerCharSet = string.printable.replace(string.ascii_lowercase, '').replace('*', '')
         if sys.argv[1] == "--emails":
             fillerCharSet = fillerCharSet.replace('_', '').replace('.', '').replace('@', '')
         dbreacher = dbreacher_impl_binary_search.DBREACHerImpl(control, table, num_secrets, maxRowSize, fillerCharSet, ord('*'))
+
+        startRound = time.time()
 
         attacker = decision_attacker_binary.decisionAttacker(dbreacher, guesses)
         while not success:
@@ -80,6 +83,5 @@ for num_secrets in secrets_to_try:
         for guess, score_tuple in refScores:
             label = 1 if guess in correct_guesses else 0
             print(str(label)+","+str(num_secrets)+","+str(score_tuple[0])+","+str(score_tuple[1])+","+str(score_tuple[2]) +","+str(setupEnd - setupStart)+","+str((end-setupEnd)/num_secrets))
+        print("Total DB Queries This Round: " + str(dbreacher.db_count))
         print("Total time spent this round in seconds: " + str(endRound-startRound))
-endAttack = time.time()
-print("Total time running in seconds: " + str(endAttack - startAttack))
